9f0a6152b871077e5a147771683e4d71c9797fc7
SUJITH-007 <ankajalusujithkumar@gmail.com>
2026-04-29 10:50:33 +0530

    Code update 18

--- Backend/server.js
+++ Backend/server.js
@@ -14,15 +14,8 @@ const playRoutes = require("./routes/playRoutes");
 const albumRoutes = require("./routes/albumRoutes");
 const app = express();
 
-// app.use(cors({
-//   origin: "http://localhost:3000", 
-//   credentials: true
-// }));
 app.use(cors({
-  origin: [
-    // "http://localhost:3000",
-    "https://internship-project-spotify-clone-gv35pgdlm.vercel.app"
-  ],
+  origin: "http://localhost:3000", 
   credentials: true
 }));
 
